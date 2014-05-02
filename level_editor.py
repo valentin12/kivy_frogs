961762c171dcdfcaea37d8c83ca01717ede953fa
Valentin Pratz <vp.pratz@yahoo.de>
2014-05-02 21:19:02 +0200

    Added switch lily and menu to the level editor

--- level_editor.py
+++ level_editor.py
@@ -4,6 +4,9 @@ from kivy.uix.scatter import Scatter
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
 from os.path import isfile
 
 from kivy.lang import Builder
@@ -13,7 +16,10 @@ Builder.load_file("editor.kv")
 
 class LevelEditorWidget(Widget):
     def export_level(self, path):
-        out = "level boats=0 flys=4 "
+        out = "level boats={} flys={} energy={} ".format(
+            self.sidebar.level_settings.boats_count.text,
+            self.sidebar.level_settings.flys_count.text,
+            self.sidebar.level_settings.energy_count.text)
         # get level size
         max_x = 0
         max_y = 0
@@ -28,10 +34,18 @@ class LevelEditorWidget(Widget):
         out += "size={},{}\n".format(max_x + 1, max_y + 1)
         # add the objects
         for c in self.level.children:
+            extra_opts = " "
             if type(c) == WaterLilyPH:
                 tp = "waterlily"
             elif type(c) == StoneLilyPH:
                 tp = "stonelily"
+            elif type(c) == SwitchLilyPH:
+                if c.options.controlled:
+                    tp = "switchlily"
+                    extra_opts += "controlled={}".format(
+                        c.options.controlled.id)
+                else:
+                    tp = "stonelily"
             elif type(c) == StartPH:
                 tp = "start"
             elif type(c) == EndPH:
@@ -41,8 +55,8 @@ class LevelEditorWidget(Widget):
             x = int(round(c.center_x / dp(100), 0))
             y = int(round(c.y / dp(100), 0))
             id = c.id
-            s = "{} pos={},{} id={}\n".format(tp, x, y, id)
-            out += s
+            s = "{} pos={},{} id={}".format(tp, x, y, id)
+            out += s + extra_opts + "\n"
         # Add the player frog
         out += "frog id=player jump_img={} sit_img={} player=True place=start\n"\
             .format(
@@ -100,9 +114,10 @@ class LevelScatter(Scatter):
     def add_widget(self, widget, index=0):
         """Override to set object ids"""
         if type(widget) in [WaterLilyPH,
-                            StoneLilyPH]:
-            widget.id = "object_{}".format(
-                self.app.editor.object_count)
+                            StoneLilyPH,
+                            SwitchLilyPH]:
+            widget.id = "object_%03d" %\
+                self.app.editor.object_count
             self.app.editor.object_count += 1
         super(LevelScatter, self).add_widget(widget, index)
 
@@ -118,6 +133,10 @@ class ToolBar(Widget):
     pass
 
 
+class SideBar(Widget):
+    pass
+
+
 class BackgroundPH(Image):
     pass
 
@@ -125,25 +144,51 @@ class BackgroundPH(Image):
 class PHScatter(Scatter):
     """Base class for placeholders"""
     current_touch = None
+    option_type = None
+    options = None
+
+    def __init__(self, **kwargs):
+        super(PHScatter, self).__init__(**kwargs)
+        if self.option_type is not None:
+            self.options = self.option_type(obj=self)
 
     def on_transform_with_touch(self, touch):
         self.current_touch = touch
+        self.app.editor.select.center = self.center
         if not self.moved:
-            index = 0
-            if type(self) == FrogPH:
-                index = 2
-            self.parent.add_widget(type(self)(pos=self.parent.pos),
-                                   index)
+            self.parent.add_widget(type(self)(pos=self.parent.pos))
             self.moved = True
             self.parent.remove_widget(self)
             self.app.editor.level.add_widget(self)
             self.center_x = touch.pos[0]
             self.center_y = touch.pos[1]
 
+    def on_touch_down(self, touch):
+        if not self.collide_point(*touch.pos):
+            return super(PHScatter, self).on_touch_down(touch)
+        self.app.editor.select.center = self.center
+        if not self.app.editor.sidebar.object_content.children\
+           == [self.options]:
+            self.app.editor.sidebar.object_content.clear_widgets()
+            if self.options:
+                self.options.top = self.app.editor.\
+                                   sidebar.object_content.top
+                self.options.x = self.app.editor.\
+                                 sidebar.object_content.x
+                self.options.width = self.app.editor.\
+                             sidebar.object_content.width
+                self.app.editor.sidebar.object_content.add_widget(
+                    self.options)
+        return super(PHScatter, self).on_touch_down(touch)
+
     def on_touch_up(self, touch):
         if touch == self.current_touch:
             self.current_touch = None
-            if self.app.editor.delete.collide_point(*touch.pos):
+            if self.app.editor.delete.collide_point(*self.center):
+                for f in [frog for frog in self.parent.children
+                          if type(frog) == FrogPH]:
+                    if f.place == self.id:
+                        self.app.editor.level.remove_widget(f)
                 self.app.editor.level.remove_widget(self)
                 return True
             x = int(round(self.center_x / dp(100), 0))
@@ -163,6 +208,7 @@ class PHScatter(Scatter):
                     LOOP = False
             self.center_x = center_x
             self.y = y
+            self.app.editor.select.center = self.center
             return True
         return super(PHScatter, self).on_touch_up(touch)
 
@@ -175,6 +221,29 @@ class WaterLilyPH(PHScatter):
 class StoneLilyPH(PHScatter):
     """Stonelily placeholder"""
     pass
+
+
+class SwitchLilyOptions(Widget):
+    controlled = ObjectProperty(None, allownone=True)
+    last_controlled = ObjectProperty(None, allownone=True)
+    obj = ObjectProperty(None, allownone=True)
+
+    def __init__(self, **kwargs):
+        super(SwitchLilyOptions, self).__init__(**kwargs)
+        self.bind(controlled=self.on_controlled_change)
+
+    def on_controlled_change(self, instance, value):
+        if self.last_controlled:
+            self.last_controlled.source = self.std_img
+        self.last_controlled = value
+        if value:
+            self.std_img = value.source
+            value.source = self.controlled_img
+
+
+class SwitchLilyPH(PHScatter):
+    """Switchlily placeholder"""
+    option_type = SwitchLilyOptions
 
 
 class BasePH(PHScatter):
@@ -230,7 +299,7 @@ class FrogPH(PHScatter):
                    type(child) != StartPH:
                     for frog in [c for c in self.parent.children
                                  if type(c) == FrogPH]:
-                        if frog.place == child.id:
+                        if frog.place == child.id and frog != self:
                             break
                     else:
                         child.bind(pos=self.on_parent_pos)
@@ -245,3 +314,45 @@ class FrogPH(PHScatter):
     def on_parent_pos(self, instance, value):
         if instance.id == self.place:
             self.center = instance.center
+
+
+class NumericInput(TextInput):
+    def insert_text(self, substring, from_undo=False):
+        if substring.isdigit():
+            return super(NumericInput, self)\
+                .insert_text(substring, from_undo=from_undo)
+
+    def on_focus(self, instance, value):
+        if not value:
+            if self.text.strip() == "":
+                self.insert_text("0")
+        super(NumericInput, self).on_focus(instance, value)
+
+
+class SelectButton(Button):
+    binded = False
+    selected = ObjectProperty(None, allownone=True)
+
+    def __init__(self, **kwargs):
+        super(SelectButton, self).__init__(**kwargs)
+
+    def on_press(self):
+        self.selecting = True
+        if not self.binded:
+            self.app.editor.level.bind(on_touch_down=
+                                       self.on_touch_select)
+
+    def on_touch_select(self, instance, touch):
+        if self.selecting:
+            self.selecting = False
+            for o in self.app.editor.level.children:
+                if type(o) in [WaterLilyPH,
+                               StoneLilyPH,
+                               SwitchLilyPH] and o.collide_point(
+                                   *touch.pos
+                               ) and o not in self.ignore:
+                    self.selected = o
+                    return True
+            self.selected = None
+        return super(SelectButton, self).on_touch_up(touch)
+
